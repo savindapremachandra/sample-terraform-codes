6059bfb8f1dd32d7c7dffd5717d307689c03ad90
savindapremachandra <92556075+savindapremachandra@users.noreply.github.com>
2022-06-19 21:35:11 +0530

    Update simple-web-app.tf

--- 04/simple-web-app.tf
+++ 04/simple-web-app.tf
@@ -149,14 +149,14 @@ resource "aws_security_group_rule" "allow_alb_all_outbound" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_db_instance" "db_instance" {
-  allocated_storage   = 20
-  storage_type        = "standard"
-  engine              = "postgres"
-  engine_version      = "12.5"
-  instance_class      = "db.t2.micro"
-  name                = "mydb"
-  username            = "foo"
-  password            = "foobarbaz"
-  skip_final_snapshot = true
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
 }
